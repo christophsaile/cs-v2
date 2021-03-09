dfce7268088d906b6a2b032f0e13340be5c7457d
Christoph Saile <christoph.saile@googlemail.com>
2021-03-09 21:11:01 +0100

    feat: add first component

---
+++ src/components/statement/statement.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './statement.scss';
+
+class Statement extends React.Component {
+  render() {
+    return <h1>Hello World</h1>;
+  }
+}
+
+export default Statement;
