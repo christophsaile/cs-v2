f1d34edc9ebefe119226ae21d6c44902b1d4279d
Christoph Saile <christoph.saile@googlemail.com>
2021-03-14 15:20:34 +0100

    feat: add intro img

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Statement from './components/statement/statement';
 import SayHey from './components/sayHey/sayHey';
+import ChrisCam from './assets/imgs/chris-cam.png';
 
 class App extends React.Component {
   render() {
     return (
       <main>
         <SayHey />
-        <section className='page'>
+        <section className='page intro'>
           <Statement title='Hey You' />
+          <img className='intro__img' src={ChrisCam} alt='Portrait Christoph Saile' />
         </section>
         <section className='page'>
           <Statement title='Àpropos' />
