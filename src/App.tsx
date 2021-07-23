ba129e2c826ea6eeb98d3c20f4e5a35e05300aed
Christoph <christoph.saile@googlemail.com>
2021-07-23 10:23:47 +0200

    test performace issues

--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,7 @@ class App extends React.Component<Props, State> {
     );
 
     this.initLocomovitveScroll();
-    this.initAnimation();
+    //this.initAnimation();
   }
 
   private handleResize = (): void => {
