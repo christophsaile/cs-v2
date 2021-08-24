08990961b5c2c4d164018e44f740a2dbc58371c0
Christoph <christoph.saile@googlemail.com>
2021-08-24 22:23:27 +0200

    feat: change text

--- src/components/aboutme/aboutme.tsx
+++ src/components/aboutme/aboutme.tsx
@@ -9,26 +9,24 @@ class Aboutme extends React.Component<Props> {
     return (
       <article className='aboutme' {...setScroll(true, -1, 'vertical')}>
         <section className='aboutme__content'>
-          <div className='aboutme__text' {...setScroll(true)}>
-            <p>
-              i like to explore everything that has to do with web development and love to create
-              websites that step a little out of the ordinary.
-            </p>
-            <p>
-              currently, i live in tübingen and study online media (bsc.) at the university of
-              furtwangen. besides my studies, i am a working student at an international, digital
-              agency as frontend developer.
-            </p>
-            <p>
-              if you have any questions or want to create something unique with me, do not hesitate
-              to get in touch with me.
-            </p>
-            <p>
-              <Link to='/about' className='styled upper arrow'>
-                contact me
-              </Link>
-            </p>
-          </div>
+          <h3 {...setScroll(true)}>about</h3>
+          <p>
+            currently, i live in tübingen and study online media (bsc.) at the university of
+            furtwangen (faculty of digital media).
+          </p>
+          <p>
+            besides my studies, i am a working student at an international, digital agency as
+            frontend developer.
+          </p>
+          <p>
+            if you have any questions or want to create something unique with me, do not hesitate to
+            get in touch with me.
+          </p>
+          <p>
+            <Link to='/about' className='styled upper arrow'>
+              contact me
+            </Link>
+          </p>
         </section>
       </article>
     );
--- src/components/creations/creations.tsx
+++ src/components/creations/creations.tsx
@@ -5,7 +5,11 @@ class Creations extends React.Component {
   render() {
     return (
       <section className='creations' {...setScroll(true, -1, 'vertical')}>
-        <h2 {...setScroll(true)}>projects</h2>
+        <h3 {...setScroll(true)}>projects</h3>
+        <p>
+          i like to explore everything that has to do with web development and love to create
+          websites that step a little out of the ordinary.
+        </p>
         <p>
           besides web development, i like to design user interfaces, create meaningful animations
           and learn more about photography.
